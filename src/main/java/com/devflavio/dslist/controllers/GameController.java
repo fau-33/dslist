0b35e9694cf7cb519cf47b5e433539c3d9c4796f
Flávio Félix <fau_felix30@hotmail.com>
2024-11-11 22:55:59 -0300

    Feito properties, entity e game

---
+++ src/main/java/com/devflavio/dslist/controllers/GameController.java
@@ -0,0 +1,26 @@
+package com.devflavio.dslist.controllers;
+
+import com.devflavio.dslist.dto.GameMinDTO;
+import com.devflavio.dslist.entities.Game;
+import com.devflavio.dslist.services.GameService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+  @Autowired
+  private GameService gameService;
+
+  @GetMapping
+  public List<GameMinDTO> findAll() {
+    List<GameMinDTO> result = gameService.findAll();
+    return result;
+
+  }
+
+}
---
+++ src/main/java/com/devflavio/dslist/repositories/GameRepository.java
@@ -0,0 +1,8 @@
+package com.devflavio.dslist.repositories;
+
+import com.devflavio.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}
