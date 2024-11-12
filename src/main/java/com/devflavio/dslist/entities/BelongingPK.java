ebf8437b3aabf27c150361175c4d3b337bfbf469
Flávio Félix <fau_felix30@hotmail.com>
2024-11-12 20:25:53 -0300

    Domain model

---
+++ src/main/java/com/devflavio/dslist/entities/BelongingPK.java
@@ -0,0 +1,61 @@
+package com.devflavio.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.Objects;
+
+@Embeddable
+public class BelongingPK {
+
+  @ManyToOne
+  @JoinColumn(name = "game_id")
+  private Game game;
+
+  @ManyToOne
+  @JoinColumn(name = "list_id")
+  private GameList gameList;
+
+  public BelongingPK() {
+
+  }
+
+  public BelongingPK(Game game, GameList gameList) {
+    this.game = game;
+    this.gameList = gameList;
+  }
+
+  public Game getGame() {
+    return game;
+  }
+
+  public void setGame(Game game) {
+    this.game = game;
+  }
+
+  public GameList getGameList() {
+    return gameList;
+  }
+
+  public void setGameList(GameList gameList) {
+    this.gameList = gameList;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    if (this == object) {
+      return true;
+    }
+    if (object == null || getClass() != object.getClass()) {
+      return false;
+    }
+    BelongingPK that = (BelongingPK) object;
+    return Objects.equals(game, that.game) && Objects.equals(gameList,
+        that.gameList);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(game, gameList);
+  }
+}
